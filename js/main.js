4708a6eee8b20a9a93df62b91d43f3f5c1d8f8bd
prathamesh111 <prathamsamudra@gmail.com>
2020-02-06 17:42:24 +0530

    different methods and api use

--- js/main.js
+++ js/main.js
@@ -1,38 +1,72 @@
-$(document).ready(function(){
-    $("#btn").click(function(){
-      $.get("https://reqres.in/api/unknown", function(datae, status){
-        console.log(datae);
-       datae.data.forEach(dt => {
-        $('#tdata').append("<tr>"+
-        "<td>"+dt.id+ "</td>"+
-        "<td>"+dt.name+ "</td>"+
-        "<td>"+dt.year + "</td>"+
-        "<td>"+dt.color+ "</td>"
-        
-        +"</tr>");
-       });
-        
-      });
-    });
-  });
-
-// var xhr = new XMLHttpRequest();
-// xhr.open("get", "https://reqres.in/api/users", true);
-// xhr.onload = function(){
-//     // console.log(xhr.responseText);
-//     var plainText=JSON.parse(xhr.responseText);
-//     // console.log(plainText.data);
-//     plainText.data.forEach(dt => {
+// $(document).ready(function(){
+//     $("#btn").click(function(){
+//       $.get("https://reqres.in/api/unknown", function(datae, status){
+//         console.log(datae);
+//        datae.data.forEach(dt => {
 //         $('#tdata').append("<tr>"+
 //         "<td>"+dt.id+ "</td>"+
-//         "<td>"+dt.email+ "</td>"+
-//         "<td>"+dt.first_name+ "</td>"+
-//         "<td>"+dt.last_name+ "</td>"+
-//         "<td>"+dt.avatar+ "</td>"
+//         "<td>"+dt.name+ "</td>"+
+//         "<td>"+dt.year + "</td>"+
+//         "<td>"+dt.color+ "</td>"
+//         +"</tr>"
         
-//         +"</tr>");
-//        });   
+//         );
+       
+//     });
+        
+//       });
+//     });
+//   });
+
+var xhr = new XMLHttpRequest();
+xhr.open("get", "https://reqres.in/api/users", true);
+xhr.onload = function(){
+    // console.log(xhr.responseText);
+    var plainText=JSON.parse(xhr.responseText);
+    // console.log(plainText.data);
+    plainText.data.forEach(dt => {
+        $('#tdata').append("<tr>"+
+        "<td>"+dt.id+ "</td>"+
+        "<td>"+dt.email+ "</td>"+
+        "<td>"+dt.first_name+ "</td>"+
+        "<td>"+dt.last_name+ "</td>"+
+        "<td>"+dt.avatar+ "</td>"
+        
+        +"</tr>");
+       });   
     
-// };
-// xhr.send();
+};
+xhr.send();
+
+function petTemplate(pet){
+    return
+    `
+    <h1>${pet.first_name}</h1>
+    `
+}
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://reqres.in/api/users?page=2", true);
+xhr.onload = function(){
+    // console.log(xhr.responseText);
+    var userArray=JSON.parse(xhr.responseText).data;
+    userArray.forEach(dt => {
+        $(".user-wrapper").append(
+          "<div class='user-info-wrapper'>"+ 
+          "<div class='left-section'>" + 
+            "<div class='first_name'>"+ dt.first_name+ "</div>"+
+            
+        "</div>"+
+        "<div class='right-section'>"
+                +
+                "<div>"+ dt.last_name+ "</div>"+
+                "<div>"+ dt.avatar+ "</div>"+
+            "</div>"+
+        "</div>"
+            
+        )
+    });
+};
+xhr.send();
+		
 
